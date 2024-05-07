35cca158d9ea2c0dc9197f75f9a66fb5e743d875
Cristián Maureira-Fredes <Cristian.Maureira-Fredes@qt.io>
2024-05-07 11:36:22 +0200

    Add footer

--- assets/js/display.js
+++ assets/js/display.js
@@ -2,6 +2,7 @@ const FILES =[
     'about.html',
     //'sponsors.html',
     'past_events.html',
+    'footer.html',
     ]
 for(let i=0; i<FILES.length;i++){
     await fetch('assets/sections/' + FILES[i]).then(r => r.text()).then(r => document.body.innerHTML+=r)
