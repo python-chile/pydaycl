17241c0076d13149f578eab07daa0494ea26982c
Aldo Caneo <aldo.caneo@pythonchile.cl>
2024-06-12 16:51:00 -0400

    New date for Vaparaiso and add psf logo

--- assets/js/valparaiso.js
+++ assets/js/valparaiso.js
@@ -7,7 +7,7 @@ const FILES =[
     '../footer.html'
     ]
 for(let i=0; i<FILES.length;i++){
-    await fetch('assets/sections/valparaiso/' + FILES[i] + '?v=1.2').then(r => r.text()).then(r => document.body.innerHTML+=r)
+    await fetch('assets/sections/valparaiso/' + FILES[i] + '?v=1.3').then(r => r.text()).then(r => document.body.innerHTML+=r)
 }
 import schedule from '/assets/js/valparaiso/schedule.js?v=1.0';
 schedule()
